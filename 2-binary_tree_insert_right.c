bb832c3b20bf4712f9eac3e3863c6e8b3508ebd5
Sgechuki <sgechuki@outlook.com>
2024-01-02 09:50:57 -0800

    Task 0-8: Node transversal

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: pointer to the created node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{	
+	binary_tree_t *node;
+
+	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right = node;
+		node->right->parent = node;
+	}
+	else
+		parent->right = node;
+	return (node);
+}
