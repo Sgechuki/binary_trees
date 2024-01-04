81a7810a82da0ba24ac8df9cba5e6f2981b769de
Sgechuki <sgechuki@outlook.com>
2024-01-04 12:08:41 -0800

    Find depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,26 +3,6 @@
 #include <stdlib.h>
 
 /**
- * maxim - implementation of max function
- * @a: value
- * @b: value
- * Return: max value
- */
-size_t maxim(size_t a, size_t b)
-{
-	if (a > b)
-	{
-		return (a);
-	}
-	else if (a < b)
-	{
-		return (b);
-	}
-	else
-		return (b);
-}
-
-/**
  * binary_tree_depth  - measures the depth of a binary tree
  * @tree: pointer to the root node of the tree to measure the depth
  * Return: if tree is NULL return 0
@@ -30,11 +10,8 @@ size_t maxim(size_t a, size_t b)
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t l_depth, r_depth;
-
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-	l_depth = binary_tree_depth(tree->left);
-	r_depth = binary_tree_depth(tree->right);
-	return (maxim(l_depth, r_depth) + 1);
+	else
+		return (1 + binary_tree_depth(tree->parent));
 }
